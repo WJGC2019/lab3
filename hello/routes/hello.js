4f68a4b09360c391f36981c23c76b055ca8f8327
WJGC2019 <46573265+WJGC2019@users.noreply.github.com>
2019-01-24 10:44:47 -0800

    Update hello.js

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,13 @@
  */
 
 exports.view = function(req, res){
+
+	var nameToShow = req.params.userName;
+	if(req.params.userName.length == 0){
+		nameToShow = "World!"
+	}
+	console.log("name is " + nameToShow)
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
